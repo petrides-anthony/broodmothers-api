31a054f37675cdd25df217f84a877430dbc9e0c8
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2020-03-18 09:08:06 +0000

    refactor and add plain string response

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # broodmothers-api
+
+### Running the server
+```
+yarn
+node index.js
+```
+### Hitting the server
+```
+http://localhost:3000/url
+```
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get("/url", (req, res, next) => {
+// Respond with string
+app.get('/my-url', (req, res) => res.send('Hello World!'))
+
+// Response with json
+app.get("/my-url/json", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+app.listen(port, () => console.log(`Server running and listening on port ${port}!`));
